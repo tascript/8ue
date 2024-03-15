8ccfaed873a05e4da4c720933ab9c3bab676ba46
tascript <wataruscript8611@gmail.com>
2024-03-15 21:55:47 +0900

    prepare scratch

---
+++ scratch/go.mod
@@ -0,0 +1,3 @@
+module github.com/tascript/8ue/scratch
+
+go 1.22.0
---
+++ scratch/socket.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+//go:wasmimport wasi_snapshot_preview1 sock_recv
+//go:noescape
+func sock_recv(fd int32, ri_data unsafe.Pointer, ri_data_len int32, ri_flags int32, ro_data_len int32, ro_flags unsafe.Pointer) syscall.Errno
+
+//go:wasmimport wasi_snapshot_preview1 sock_
+//go:noescape
+func sock_send(fd int32, si_data unsafe.Pointer, si_data_len int32, si_flags int32, ret_data_len int32) syscall.Errno
