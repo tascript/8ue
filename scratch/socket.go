03f0c7fda22ba2bb27013cfb69c740703f20ac84
tascript <wataruscript8611@gmail.com>
2024-03-16 12:13:20 +0900

    rename func

--- scratch/socket.go
+++ scratch/socket.go
@@ -9,6 +9,6 @@ import (
 //go:noescape
 func sock_recv(fd int32, ri_data unsafe.Pointer, ri_data_len int32, ri_flags int32, ro_data_len int32, ro_flags unsafe.Pointer) syscall.Errno
 
-//go:wasmimport wasi_snapshot_preview1 sock_
+//go:wasmimport wasi_snapshot_preview1 sock_send
 //go:noescape
 func sock_send(fd int32, si_data unsafe.Pointer, si_data_len int32, si_flags int32, ret_data_len int32) syscall.Errno
